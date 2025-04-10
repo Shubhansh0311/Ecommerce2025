5a3602b666c25a9cc4e86dce5d98365b8aa68506
shubhansh0311 <shubhanshagrawal0311@gmail.com>
2025-04-11 00:11:56 +0530

    test-2

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,7 +4,7 @@ dotenv.config()
 const dbConnect=async()=>{
    
         
-       const db=await mongoose.connect(process.env.DATABASE_URL||"mongodb+srv://shubhansh:shubhansh@cluster0.ia7xyfr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Ecommerce"
+       const db=await mongoose.connect("mongodb+srv://shubhansh:shubhansh@cluster0.ia7xyfr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Ecommerce"
 ).then((success)=>{
     console.log("database connected successfully",success.connection.name);
     
@@ -12,3 +12,19 @@ const dbConnect=async()=>{
         console.log("error while establishing connetion ",err)}
     )}
 export default dbConnect
+
+
+// import mongoose from "mongoose";
+// import dotenv from "dotenv"
+// dotenv.config()
+// const dbConnect=async()=>{
+   
+        
+//        const db=await mongoose.connect(process.env.DATABASE_URL||"mongodb+srv://shubhansh:shubhansh@cluster0.ia7xyfr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Ecommerce"
+// ).then((success)=>{
+//     console.log("database connected successfully",success.connection.name);
+    
+// }).catch ((err)=>{
+//         console.log("error while establishing connetion ",err)}
+//     )}
+// export default dbConnect
